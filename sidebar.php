4b264969ca576058bc06e8f5b0aeacf8b690a3ca
Shinmachi Kazuki <swc1250@gmail.com>
2018-07-26 21:11:25 +0900

    First commit.

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<section class="contents-sub">
+<nav>
+<figure>
+	<img src="<?php echo get_template_directory_uri(); ?>/images/banner_facility.png" alt="施設紹介" width="220" height="164">
+	<figcaption>充実した医療施設のご紹介</figcaption>
+</figure>
+</nav>
+</section>
